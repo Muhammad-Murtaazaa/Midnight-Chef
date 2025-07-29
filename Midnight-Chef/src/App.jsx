98baa0843a66e79963bbc4ee9bf60f557a4ade0d
Muhammad Murtaza <muhammad.murtaazaa@gmail.com>
2025-07-29 12:26:46 +0500

    Added the input form

--- Midnight-Chef/src/App.jsx
+++ Midnight-Chef/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from './components/Header.jsx'
+import Form from './components/Form.jsx'
 const App = () => {
   return (
     <>
      <Header />
+     <Form />
     </>
   )
 }
---
+++ Midnight-Chef/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Form.css'
+const Form = () => {
+  return (
+    <main>
+        <form>
+            <input type="text" placeholder='Add Ingredients' aria-label='Add Ingredients' />
+            <button>Add Ingredients</button>
+            </form>
+    </main>
+  )
+}
+
+export default Form
