003902e75c470dfb3da320d42a92793b4dac34a2
Muhammad Murtaza <muhammad.murtaazaa@gmail.com>
2025-07-31 09:45:09 +0500

    Updated the form jsx

--- Midnight-Chef/src/components/Form.jsx
+++ Midnight-Chef/src/components/Form.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './Form.css'
 const Form = () => {
+
+  const [ingredients, setIngredients] = React.useState([]);
+  const listofIngredients = ingredients.map((ingredient, index) => {
+    return <li key={index}>{ingredient}</li>
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient])
+  }
+
   return (
     <main>
-        <form>
-            <input type="text" placeholder='Add Ingredients' aria-label='Add Ingredients' />
-            <button>Add Ingredients</button>
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="ingredient" placeholder='Add Ingredients' aria-label='Add Ingredients' />
+            <button type="submit">Add Ingredients</button>
             </form>
+            <ul>
+              {listofIngredients.length > 0 ? listofIngredients : <li>No ingredients added yet</li>}
+            </ul>
     </main>
   )
 }
