8a73846ca0fe43ee6e32389fb0bcaf21b4ca34e5
Muhammad Murtaza <muhammad.murtaazaa@gmail.com>
2025-08-02 21:23:54 +0500

    Implemented some conditional rendering and ui for the get receipe

--- Midnight-Chef/src/components/Form.jsx
+++ Midnight-Chef/src/components/Form.jsx
@@ -17,9 +17,19 @@ const Form = () => {
             <input type="text" name="ingredient" placeholder='Add Ingredients' aria-label='Add Ingredients' />
             <button type="submit">Add Ingredients</button>
             </form>
+            <section>
+              <h2>Ingredients List</h2>
             <ul>
               {listofIngredients.length > 0 ? listofIngredients : <li>No ingredients added yet</li>}
             </ul>
+            {listofIngredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>
     </main>
   )
 }
