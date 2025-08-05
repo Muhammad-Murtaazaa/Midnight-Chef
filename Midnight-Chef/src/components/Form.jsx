f27f01b9a16f8fd757c35a0726dded829dbcf25a
Muhammad Murtaza <muhammad.murtaazaa@gmail.com>
2025-08-05 21:27:55 +0500

    Added funtionality to render the receipe from AI

--- Midnight-Chef/src/components/Form.jsx
+++ Midnight-Chef/src/components/Form.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import './Form.css'
 import Receipe from './receipe.jsx'
-import { getRecipeFromChefClaude, getRecipeFromMistral } from "./AI.js"
+import { getRecipeFromMistral } from "./AI.js"
 const Form = () => {
 
   const [ingredients, setIngredients] = React.useState([]);
-  const [recipeshown, setRecipesShown] = React.useState(false);
+  const [recipeshown, setRecipesShown] = React.useState("");
   const listofIngredients = ingredients.map((ingredient, index) => {
     return <li key={index}>{ingredient}</li>
   });
   const handleSubmit = (formData) => {
     const newIngredient = formData.get("ingredient")
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient])
+  }
+  async function getRecipe()
+  {
+     const recipeMarkdown = await getRecipeFromMistral(ingredients)
+     setRecipesShown(recipeMarkdown)
   }
 
   return (
@@ -30,10 +35,10 @@ const Form = () => {
                         <h3>Ready for a recipe?</h3>
                         <p>Generate a recipe from your list of ingredients.</p>
                     </div>
-                    <button onClick={() => setRecipesShown(prev => !prev)}>Get a recipe</button>
+                    <button onClick={() => getRecipe()}>Get a recipe</button>
                 </div>}
                 <section>
-    { recipeshown && <Receipe /> }
+    { recipeshown && <Receipe receipe = {recipeshown} /> }
 </section>
             </section>
     </main>
